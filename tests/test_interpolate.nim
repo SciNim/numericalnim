cb63dcc985787ac3d191f4f0c61084e6e7cbfbe8
HugoGranstrom <hugogranstrom@outlook.com>
2019-10-29 16:23:40 +0100

    improve readability

--- tests/test_interpolate.nim
+++ tests/test_interpolate.nim
@@ -6,23 +6,23 @@ proc f(x: float): float = sin(x)
 let t = linspace(0.0, 10.0, 100)
 let y = t.map(f)
 let spline = newCubicSpline(t, y)
-let tTest = arange(0.0, 10.0, 0.2345, true, false)
+let tTest = arange(0.0, 10.0, 0.2345, includeStart=true, includeEnd=false)
 let yTest = tTest.map(f)
 let splineProc = spline.toProc
 
-test "Eval in input points, direct":
+test "Spline Eval in input points, direct":
     let res = spline.eval(t)
     for i, val in res:
         check isClose(val, y[i], 1e-15)
 
-test "Eval in input points, for loop":
+test "Spline Eval in input points, for loop":
     var res = newSeq[float](t.len)
     for i, tTemp in t:
         res[i] = spline.eval(tTemp)
     for i, val in res:
         check isClose(val, y[i], 1e-15)
     
-test "Eval between input points":
+test "Spline Eval between input points":
     let res = spline.eval(tTest)
     for i, val in res:
         check isClose(val, yTest[i], 1e-4)
@@ -34,7 +34,7 @@ test "Spline.toProc, single value":
     for i, val in res:
         check isClose(val, y[i], 1e-15)
 
-test "Integrate using adaptiveSimpson, implicit":
+test "Spline Integrate using adaptiveSimpson, implicit":
     let computedValue = adaptiveSimpson(spline, 0.0, 7.5)
     let correct = -cos(7.5) + cos(0.0)
     check isClose(computedValue, correct, tol=1e-7)
@@ -48,8 +48,3 @@ test "Spline derivEval, seq input":
     let res = spline.derivEval(tTest)
     for i, val in res:
         check isClose(val, cos(tTest[i]), 1e-3)
-
-
-
-
-
